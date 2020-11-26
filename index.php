e16f5045389c1ddb5ac8b7aca09bfbfc1db5abd6
faizalfebriyanto886 <faizalfebriyanto886@gmail.com>
2020-11-26 23:41:15 +0700

    faizal

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dodol Action Figure</title>
+	<link rel="stylesheet" type="text/css" href="css/style2.css">
+</head>
+<body>
+	<div id="header">		
+		<ul id="navigation">
+			<li class="selected">
+				<a href="index.php">home</a>
+			</li>
+			<li>
+				<a href=".php">product</a>
+			</li>
+			<li>
+				<a href=".php">cart</a>
+			</li>
+			<li>
+				<a href=".php">orders</a>
+			</li>
+      <li>
+        <a href=".php">contact</a>
+      </li>
+		</ul>
+	</div>
+	<div id="body">
+		<div id="featured">
+			<img src="images/welcum2.jpg" alt="">
+			<div>
+				<h2>Selamat Datang di Dodol Action Figure</h2>
+        <span>Kami menjual Action Figure yang murah dan berkualitas.</span>
+        <span>Fast respon dan pengiriman cepat.</span>
+        <a href="">Beli Sekarang!</a>
+			</div>
+		</div>
+		<ul>
+			<li>
+				<a href=".php">
+					<img src="images/products/produk1.jpg" alt="">
+					<span>Dante Devil May Cry 5</span>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/products/produk2.jpg" alt="">
+					<span>tajiro kamado demon slayer</span>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/products/produk3.jpg" alt="">
+					<span>kimetsu no yaiba</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<p><b>CARA PEMBELIAN</b></p>
+	<ol>
+		<li> Silahkan <a href="">buat akun</a> terlebih dahulu. Skip step ini jika anda sudah mempunyai akun. </li>
+		<li> Silahkan melakukan <a href="">login</a> jika anda sudah mendaftarkan akun anda. </li>
+		<li> Pilih <a href="">produk</a> yang anda inginkan, lalu klik add to cart. </li>
+		<li> Produk yang anda pilih akan masuk ke <a href="">troli</a> pembelian, silahkan konfirmasi jumlah yang akan dibeli lalu klik tombol purchase.</li>
+		<li> Produk yang sudah dibeli akan masuk di bagian <a href="">pembelian</a>. </li>
+		<li> Transaksi akan di proses oleh admin. </li>
+	</ol>
+	<footer style="margin-top:50px;">
+		<p style="text-align:center; font-size:0.8em;clear:both;">&copy; Dodol Action Figure Shop. All Rights Reserved.</p>
+	</footer>
+</body>
+</html>
